8302ab62090d647ff8e9b52edd93c604b1021238
extjohny <laind105@mail.ru>
2026-05-24 12:37:51 +0300

    add retrofit dependency

--- feature-auth-impl/build.gradle.kts
+++ feature-auth-impl/build.gradle.kts
@@ -34,6 +34,10 @@ android {
 }
 
 dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     implementation(libs.dagger)
     ksp(libs.dagger.compiler)
 
