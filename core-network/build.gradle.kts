24566abf5f7e459ad3d417bb7492edec6f7c0f51
extjohny <laind105@mail.ru>
2026-05-29 16:15:03 +0300

    configure di network module

---
+++ core-network/build.gradle.kts
@@ -0,0 +1,61 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "ru.abdulkhalikow.core_network"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+}
+
+dependencies {
+
+    implementation(project(":feature-auth-api"))
+    implementation(project(":core-utils"))
+
+
+    implementation(libs.dagger)
+    kapt(libs.dagger.compiler)
+
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
--- core-network/src/main/java/ru/abdulkhalikow/core_network/NetworkModule.kt
+++ core-network/src/main/java/ru/abdulkhalikow/core_network/NetworkModule.kt
@@ -1,4 +1,38 @@
 package ru.abdulkhalikow.core_network
 
-class NetworkModule {
+import dagger.Module
+import dagger.Provides
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.abdulkhalikow.core_utils.AuthInterceptor
+import ru.abdulkhalikow.core_utils.Constants.JAMENDO_BASE_URL
+import javax.inject.Singleton
+
+@Module
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(@AuthInterceptor authInterceptor: Interceptor): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .addInterceptor(authInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(JAMENDO_BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
